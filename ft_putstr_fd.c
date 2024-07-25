73a43117c28a0e55a9757ddc4b1ea1e1a5ac3d9d
Ubuntu <ubuntu@vps-866f53e7.vps.ovh.net>
2024-07-25 00:24:16 +0000

    finished

---
+++ ft_putstr_fd.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevisout <kevisout@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/25 00:20:59 by kevisout          #+#    #+#             */
+/*   Updated: 2024/07/25 00:23:51 by kevisout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+ft_putstr_fd ecris la chaine de caractere 's' sur la sortie 'fd'.
+On evite de faire un boucle qui fait write() car write est un syscall.
+Et spammer un syscall c'est pas optimisee, donc on calcule la longueur de s
+pour ecrire s avec 1 seul write().
+*/
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (s)
+		i++;
+	write(fd, s, i);
+}
