5ea225aa975de9929195dedec5ebcbf41d02a21c
Ubuntu <ubuntu@vps-866f53e7.vps.ovh.net>
2024-07-25 00:27:45 +0000

    finished

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: kevisout <kevisout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 00:20:59 by kevisout          #+#    #+#             */
-/*   Updated: 2024/07/25 00:23:51 by kevisout         ###   ########.fr       */
+/*   Updated: 2024/07/25 00:27:00 by kevisout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,10 @@ void	ft_putstr_fd(char *s, int fd)
 {
 	int	i;
 
+	if (!s)
+		return ;
 	i = 0;
-	while (s)
+	while (s[i])
 		i++;
 	write(fd, s, i);
 }
