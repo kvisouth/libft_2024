9a9e4ec22ea4d220653e9883faae4c2a1e4c26ff
Ubuntu <ubuntu@vps-866f53e7.vps.ovh.net>
2024-07-25 22:37:56 +0000

    finished

---
+++ ft_split.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevisout <kevisout@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/25 19:57:05 by kevisout          #+#    #+#             */
+/*   Updated: 2024/07/25 22:37:46 by kevisout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/* Retourne le nombre de mots dont split a besoin pour malloc */
+int	count_words(char const *s, char sep)
+{
+	int	cpt;
+	int	i;
+
+	i = 0;
+	cpt = 0;
+	while (s[i])
+	{
+		while (s[i] == sep && s[i])
+			i++;
+		if (s[i] != sep && s[i])
+			cpt++;
+		while (s[i] != sep && s[i])
+			i++;
+	}
+	return (cpt);
+}
+
+/* Retourne le mot que split doit ranger dans son tableau */
+char	*dup_str(char const *s, char sep)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	while (s[i] != sep)
+		i++;
+	str = malloc(i + 1);
+	if (!str)
+		return (NULL);
+	j = 0;
+	while (j < i)
+	{
+		str[j] = s[j];
+		j++;
+	}
+	str[j] = '\0';
+	return (str);
+}
+
+/*
+ft_split decoupe une string pour ranger chaque "mot" dans un tableau
+(char *). Chaque mot est separee par le separateur 'c'.
+*/
+char	**ft_split(char const *s, char c)
+{
+	int		i;
+	char	**tab;
+
+	i = 0;
+	tab = malloc((count_words(s, c) + 1) * sizeof(char *));
+	if (!tab)
+		return (NULL);
+	while (*s)
+	{
+		while (*s == c && *s)
+			s++;
+		if (*s != c && *s)
+		{
+			tab[i] = dup_str(s, c);
+			if (!tab)
+				return (NULL);
+			i++;
+		}
+		while (*s != c && *s)
+			s++;
+	}
+	tab[i] = NULL;
+	return (tab);
+}
