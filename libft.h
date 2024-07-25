fa3e6dcde2e2b2f56c06d669636b7199da6ce48a
Ubuntu <ubuntu@vps-866f53e7.vps.ovh.net>
2024-07-25 22:39:36 +0000

    added fcts

---
+++ libft.h
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevisout <kevisout@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/25 12:19:08 by kevisout          #+#    #+#             */
+/*   Updated: 2024/07/25 21:43:26 by kevisout         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+/* LIBFT */
+# include <stddef.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdint.h>
+
+/* DEBUG */
+# include <stdio.h>
+# include <string.h>
+
+/* MEMORY FUNCTIONS */
+void	*ft_calloc(size_t nmemb, size_t size);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
+/* ASCII FUNCTIONS  */
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int c);
+int		ft_isprint(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
+
+/* WRITE FUNCTIONS */
+void    ft_putchar_fd(char c, int fd);
+void    ft_putstr_fd(char *s, int fd);
+void    ft_putendl_fd(char *s, int fd);
+void    ft_putnbr_fd(int n, int fd);
+
+/* STRING FUNCTIONS */
+char    *ft_itoa(int n);
+int     ft_atoi(const char *str);
+char	**ft_split(char const *s, char c);
+
+/* LISTS */
+
+#endif
