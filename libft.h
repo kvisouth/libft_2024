1500fe3ff23772670274b4e011e5bd808dc95d98
kvisouth <visouthivong.kevin@gmail.com>
2024-08-17 14:45:47 +0200

    normed

--- libft.h
+++ libft.h
@@ -23,6 +23,13 @@
 # include <stdio.h>
 # include <string.h>
 
+/* LINKED LIST DATA STRUCTURE */
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
 /* MEMORY FUNCTIONS */
 void	*ft_calloc(size_t nmemb, size_t size);
 void	ft_bzero(void *s, size_t n);
@@ -42,14 +49,14 @@ int		ft_tolower(int c);
 int		ft_toupper(int c);
 
 /* WRITE FUNCTIONS */
-void    ft_putchar_fd(char c, int fd);
-void    ft_putstr_fd(char *s, int fd);
-void    ft_putendl_fd(char *s, int fd);
-void    ft_putnbr_fd(int n, int fd);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 /* STRING FUNCTIONS */
-char    *ft_itoa(int n);
-int     ft_atoi(const char *str);
+char	*ft_itoa(int n);
+int		ft_atoi(const char *str);
 char	**ft_split(char const *s, char c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
@@ -68,5 +75,14 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 
 /* LISTS */
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list	*ft_lstnew(void *content);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
