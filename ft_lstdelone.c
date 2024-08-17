0535b697ce2795da848b4ae66b9086f11e72c972
Ubuntu <ubuntu@vps-866f53e7.vps.ovh.net>
2024-08-17 23:48:20 +0000

    fixed

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: kevisout <kevisout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 19:12:30 by kevisout          #+#    #+#             */
-/*   Updated: 2024/08/17 23:41:10 by kevisout         ###   ########.fr       */
+/*   Updated: 2024/08/17 23:48:11 by kevisout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 ft_lstdelone libere la supprime le contenu de 'content' et free() cet element
 */
 
-void	ft_lstdelone(t_list *lst, void(*del)(void*))
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	if (!lst)
 		return ;
